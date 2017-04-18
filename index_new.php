d3b7042ca3c9511df488dc7da694cc2b379c6a8d
Sandor Dosa <lord.sandor.dosa@gmail.com>
2017-04-18 02:28:46 -0500

    March to May 1st

--- index_new.php
+++ index_new.php
@@ -10,18 +10,7 @@ session_start();
 include "ikeqcfuncs.inc";
 include "colors.inc";
 
-// This is the splash page canidate.
-
 OpenTOP("IKEqC AS LII");
-
-//<div class="w3-row">\n";
-//<div class="w3-col m3 l2">\n";
-//<p>12 columns on a small screen, 4 on a medium screen, and 3 on a large screen.</p>\n";
-//</div>\n";
-//<div class="w3-col m8 l9">\n";
-//<p>12 columns on a small screen, 8 on a medium screen, and 9 on a large screen.</p>\n";
-//</div>\n";
-//</div>\n";
 
 print "<section class=\"w3-row w3-theme\" name=\"Banner\">\n";
   print "<article class=\"w3-col m3 l2 w3-cell w3-theme w3-center\" name=\"Logo_Block\">\n";
@@ -31,11 +20,11 @@ print "<section class=\"w3-row w3-theme\" name=\"Banner\">\n";
     print "<H1>Inter-Kingdom Equestrian Competition</H1>\n";
     print "<p>In the Fifty-Second Year of the Society of Creative Anachronism</p>\n";
       print "<div class=\"w3-bar w3-center\">\n";
-      print "<a href=\"#\" class=\"w3-bar-item w3-button w3-mobile\">Rules</a>\n";
-      print "<a href=\"#\" class=\"w3-bar-item w3-button w3-mobile\">Downloads</a>\n";
-      print "<a href=\"#\" class=\"w3-bar-item w3-button w3-mobile\">Scores Archive</a>\n";
-      print "<a href=\"#\" class=\"w3-bar-item w3-button w3-mobile\">Blog Archive</a>\n";
-      print "<a href=\"#\" class=\"w3-bar-item w3-button w3-mobile\">Made Possible by:</a>\n";
+      print "<a href=\"https://sandordosa.github.io/ikeqc_lii\" class=\"w3-bar-item w3-button w3-mobile\">Rules</a>\n";
+      print "<a href=\"http://scaikeqc.org/downloads.php\" class=\"w3-bar-item w3-button w3-mobile\">Downloads</a>\n";
+      print "<a href=\"http://scaikeqc.org/archive/index.php\" class=\"w3-bar-item w3-button w3-mobile\">Scores Archive</a>\n";
+      print "<a href=\"http://scaikeqc.org/blog/index.php\" class=\"w3-bar-item w3-button w3-mobile\">Blog</a>\n";
+      print "<a href=\"http://scaikeqc.org/btyb.php\" class=\"w3-bar-item w3-button w3-mobile\">Made Possible by:</a>\n";
       print "</div>";
   print "</article>\n"; // Title
 print "</section>\n";
@@ -44,11 +33,7 @@ print "<section class=\"w3-row w3-theme\" name=\"Main_Window\">\n";
 
   print "<article class=\"w3-col m9 l10 w3-theme\" name=\"Content\">\n";
 
-    // IF EXISTS SPECIAL NOTICE
-    print "<div class=\"w3-panel w3-theme-l1 w3-border w3-margin\">\n";
-      print "<H3>SPECIAL NOTICE:</H3>\n";
-      print "<p>[text of special notice here]</p>\n";
-    print "</div>\n";
+    include "special_notice.inc";
 
     // IF EXISTS Blog Article within date
     print "<div class=\"w3-panel w3-theme-l2 w3-border w3-margin\">\n";
@@ -60,7 +45,7 @@ print "<section class=\"w3-row w3-theme\" name=\"Main_Window\">\n";
     print "<section class=\"w3-container w3-center\" name=\"Top5_Scores_Grid\">\n";
       print "<article class=\"w3-row\">\n";
         print "<div class=\"w3-col m6 l4 w3-cell w3-theme-l3 w3-border w3-padding-small w3-center\">\n";
-          print "<p>[Game]<br>\n";
+          print "<p>Behead the Enemy - Short<br>\n";
           print "[1st]<br>\n";
           print "[2nd]<br>\n";
           print "[3rd]<br>\n";
@@ -68,7 +53,7 @@ print "<section class=\"w3-row w3-theme\" name=\"Main_Window\">\n";
           print "[5th]</p>\n";
         print "</div>\n";
         print "<div class=\"w3-col m6 l4 w3-cell w3-theme-l3 w3-border w3-padding-small w3-center\">\n";
-          print "<p>[Game]<br>\n";
+          print "<p>Behead the Enemy - Long<br>\n";
           print "[1st]<br>\n";
           print "[2nd]<br>\n";
           print "[3rd]<br>\n";
@@ -76,7 +61,7 @@ print "<section class=\"w3-row w3-theme\" name=\"Main_Window\">\n";
           print "[5th]</p>\n";
         print "</div>\n";
         print "<div class=\"w3-col m6 l4 w3-cell w3-theme-l3 w3-border w3-padding-small w3-center\">\n";
-          print "<p>[Game]<br>\n";
+          print "<p>Ring Tilt<br>\n";
           print "[1st]<br>\n";
           print "[2nd]<br>\n";
           print "[3rd]<br>\n";
@@ -84,7 +69,7 @@ print "<section class=\"w3-row w3-theme\" name=\"Main_Window\">\n";
           print "[5th]</p>\n";
         print "</div>\n";
         print "<div class=\"w3-col m6 l4 w3-cell w3-theme-l3 w3-border w3-padding-small w3-center\">\n";
-          print "<p>[Game]<br>\n";
+          print "<p>Reed Chop<br>\n";
           print "[1st]<br>\n";
           print "[2nd]<br>\n";
           print "[3rd]<br>\n";
@@ -92,7 +77,7 @@ print "<section class=\"w3-row w3-theme\" name=\"Main_Window\">\n";
           print "[5th]</p>\n";
         print "</div>\n";
         print "<div class=\"w3-col m6 l4 w3-cell w3-theme-l3 w3-border w3-padding-small w3-center\">\n";
-          print "<p>[Game]<br>\n";
+          print "<p>M.A.S.T.<br>\n";
           print "[1st]<br>\n";
           print "[2nd]<br>\n";
           print "[3rd]<br>\n";
@@ -100,7 +85,7 @@ print "<section class=\"w3-row w3-theme\" name=\"Main_Window\">\n";
           print "[5th]</p>\n";
         print "</div>\n";
         print "<div class=\"w3-col m6 l4 w3-cell w3-theme-l3 w3-border w3-padding-small w3-center\">\n";
-          print "<p>[Game]<br>\n";
+          print "<p>M.A.T.T.<br>\n";
           print "[1st]<br>\n";
           print "[2nd]<br>\n";
           print "[3rd]<br>\n";
@@ -120,7 +105,7 @@ print "<section class=\"w3-row w3-theme\" name=\"Main_Window\">\n";
       print "<p>[3rd place]</p>\n";
     print "</div>\n";
     print "<div class=\"w3-panel w3-theme-l4 w3-center\">\n";
-      print "<strong>Archive</strong>\n";
+      print "<strong>Scores Archive</strong>\n";
       print "<p>[link]</p>\n";
       print "<p>[link]</p>\n";
       print "<p>[link]</p>\n";
